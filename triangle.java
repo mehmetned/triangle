08128cdcfa22e61793ae44a60ff81d86167969da
mehmetned <yildirim84@gmail.com>
2017-03-08 16:08:05 +0200

    Update triangle.java

--- triangle.java
+++ triangle.java
@@ -1,16 +1,20 @@
 public class HelloWorld{  // arguments are passed using the text field below this editor  
   public static void main(String[] args)  {    
     Triangle triangle = new Triangle(3,3,7);    
-    System.out.print(triangle.getType());  }}// you can add other public classes to this editor in any order
+    System.out.print(triangle.getType());  
+  }
+}// you can add other public classes to this editor in any order
 public class Triangle{  
   private int firstSide;  
   private int secondSide;  
   private int thirdSide;      
-  public Triangle()  {  }    
+  public Triangle()  {  
+  }    
   public Triangle(int a,int b,int c)  {    
     firstSide = a;    
     secondSide = b;    
-    thirdSide = c;  }      
+    thirdSide = c;  
+  }      
   public void setFirstSide(int lenght)  {   
     firstSide = lenght;  
   }    
