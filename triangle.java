063480ab1abd1584b626c4a5f6ac5125ac1e0660
mehmetned <yildirim84@gmail.com>
2017-03-08 16:05:26 +0200

    Update triangle.java

--- triangle.java
+++ triangle.java
@@ -1,8 +1,1 @@
-public class HelloWorld {
-
-    public static void main(String[] args) {
-        // Prints "Hello, World" to the terminal window.
-        System.out.println("Hello, World");
-    }
-
-}
+public class HelloWorld{  // arguments are passed using the text field below this editor  public static void main(String[] args)  {    Triangle triangle = new Triangle(3,3,7);    System.out.print(triangle.getType());  }}// you can add other public classes to this editor in any orderpublic class Triangle{  private int firstSide;  private int secondSide;  private int thirdSide;      public Triangle()  {  }    public Triangle(int a,int b,int c)  {    firstSide = a;    secondSide = b;    thirdSide = c;  }      public void setFirstSide(int lenght)  {    firstSide = lenght;  }    public void setSecondSide(int lenght)  {    secondSide = lenght;  }    public void setThirdSide(int lenght)  {    thirdSide = lenght;  }  public String getType()  {    if(firstSide == secondSide && thirdSide == secondSide )      return "Equilateral";    else if(firstSide == secondSide || thirdSide == secondSide || firstSide == thirdSide)      return "Isosceles";    else       return "Scalene";  }    }
